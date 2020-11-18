0dbd2d7f2ff63045823247d00d719d97ed0baa05
sebastianMorales01 <sebastian.morales.8080@gmail.com>
2020-11-18 20:10:57 -0300

    fase final1

--- models/Conexion.php
+++ models/Conexion.php
@@ -2,10 +2,16 @@
     namespace models;
 
 class Conexion{
+    /*
+    public static $user="root";
+    public static $pass="";
+    public static $URL="mysql:host=localhost;dbname=optica_2020";
+    */
+    
     public static $user="unzdzskbuhowwfza";
     public static $pass="06b7QY15Ngz4iJflWP4I";
     public static $URL="mysql:host=bhk6hr0a52u3xsopajvx-mysql.services.clever-cloud.com;dbname=bhk6hr0a52u3xsopajvx";
-
+    
     public static function conector(){
         try{
             return new \PDO(Conexion::$URL, Conexion::$user, Conexion::$pass);
